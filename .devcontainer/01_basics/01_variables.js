29f3a46464d6da3332e6f58bb995e895dc04a6fa
arshies <162682729+arshies@users.noreply.github.com>
2024-03-09 09:58:20 +0000

    variables done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234
+let accountName = "rahul"
+var accountPass = "12345"
+accountLocation = "mumbai"
+
+console.table([accountId, accountName, accountPass, accountLocation]);
+
+// accountId = 1212
+accountName = "ramnor"
+accountPass = "121212"
+accountLocation = "nagpur"
+console.table([accountId, accountName, accountPass, accountLocation]);
